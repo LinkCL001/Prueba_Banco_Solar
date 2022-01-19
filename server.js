8e58f2188ff9b1ea68bec8a48dd0e92f4c5dcb7b
JuanQuezada <ju.quezada.c@gmail.com>
2022-01-19 01:23:17 -0300

    agregando funciones asinconas y catcha error

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-const pool = new Pool(config);
-
 const config = {
   user: "postgres",
   host: "localhost",
@@ -16,6 +14,94 @@ const config = {
   idleTimeoutMillis: 5000,
   connnectionTimeoutMillis: 2000,
 };
+
+const pool = new Pool(config);
+
+async function insertarUsuario(datos, pool) {
+    pool.connect(async (error_conexion, client, release) => {
+        if (error_conexion) return console.error(error_conexion.code);
+    const SQLQuery = {
+      text: "insert into usuarios (nombre, balance) values ( $1 , $2,) RETURNING *;",
+      values: datos,
+      rowMode: "array",
+    };
+    try {
+        const res = await client.query(SQLQuery);
+        console.log(
+          `Ultimo Usuario ${res.rows[0].nombre} agregado con éxito.`
+        );
+      } catch (error_consulta) {
+        console.log("! Error consulta !", error_consulta.code);
+      }
+      release();
+      pool.end();
+    });
+}
+
+async function listarUsuarios(pool) {
+    pool.connect(async (error_conexion, client, release) => {
+        if (error_conexion) return console.log(error_conexion.code);
+    
+        const SQLQuery = {
+          rowMode: "array",
+          text: "SELECT * FROM usuarios",
+        };
+        try {
+          const res = await client.query(SQLQuery);
+          console.log("Registro actual:", res.rows);
+        } catch (error_consulta) {
+          console.log("Error de conexion: ", error_consulta.code);
+        }
+        release();
+    
+        pool.end();
+      });
+}
+
+async function editarUsuario(datos, pool) {
+    pool.connect(async (error_conexion, client, release) => {
+      if (error_conexion) return console.log(error_conexion.code);
+  
+      const SQLQuery = {
+        name: "editarUsuario",
+        text: "UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *",
+        values: [...datos, id],
+      };
+      try {
+        const res = await client.query(SQLQuery);
+        console.log(`Usuario ${res.rows[0].id} editado con éxito`);
+      } catch (error_consulta) {
+        console.log("Error de conexion: ", error_consulta.code);
+      }
+      release();
+  
+      pool.end();
+    });
+}
+
+async function borrarUsuario(datos, pool) {
+    pool.connect(async (error_conexion, client, release) => {
+      if (error_conexion)
+        return console.log("Error de conexion: ", error_conexion.code);
+  
+      const SQLQuery = {
+        name: "eliminarUsuario",
+        text: "DELETE FROM usuarios WHERE id = $1 RETURNING *",
+        values: [datos],
+      };
+      try {
+        const res = await client.query(SQLQuery);
+        console.log(
+          `Registro de usuario con id ${res.rows[0].id} eliminado con éxito`);
+      } catch (error_consulta) {
+        console.log("Error de consulta: ", error_consulta.code);
+      }
+      release();
+  
+      pool.end();
+    });
+  }
+
 
 http
   .createServer((req, res) => {
@@ -41,33 +127,33 @@ http
       nuevoUsuario(req, res, pool);
     }
     if (req.url.startsWith("/usuario") && req.method == "PUT") {
-        const actualizarUsuario = (req, res, pool) => {
-            const params = url.parse(req.url, true).query; 
-            const id = params.id;
-            let body = "";
-            req.on("data", (e) => {
-              body += e;
-            });
-            req.on("end", async () => {
-              const data = Object.values(JSON.parse(body));
-              const codigo = await editarUsuario(data, id, pool); 
-              if (codigo > 0) {
-                res.statusCode = 200;
-                texto = "Registro editado con éxito!";
-              } else {
-                res.statusCode = 400;
-                texto = "Error para editar el registro";
-              }
-              res.end(console.log(texto));
-            });
-          };
+      const actualizarUsuario = (req, res, pool) => {
+        const params = url.parse(req.url, true).query;
+        const id = params.id;
+        let body = "";
+        req.on("data", (e) => {
+          body += e;
+        });
+        req.on("end", async () => {
+          const data = Object.values(JSON.parse(body));
+          const codigo = await editarUsuario(data, id, pool);
+          if (codigo > 0) {
+            res.statusCode = 200;
+            texto = "Registro editado con éxito!";
+          } else {
+            res.statusCode = 400;
+            texto = "Error para editar el registro";
+          }
+          res.end(console.log(texto));
+        });
+      };
       actualizarUsuario(req, res, pool);
     }
     if (req.url.startsWith("/usuario") && req.method == "DELETE") {
       const eliminarUsuario = async (req, res, pool) => {
         const params = url.parse(req.url, true).query;
         const id = params.id;
-        const codigo = await eliminarUsuario(id, pool);
+        const codigo = await borrarUsuario(id, pool);
         let texto = "";
         if (codigo > 0) {
           res.statusCode = 200;
@@ -82,17 +168,39 @@ http
     }
     if (req.url.startsWith("/usuarios") && req.method == "GET") {
       const listadoUsuarios = async (res, pool) => {
-        const registros = await listadoUsuarios(pool);
+        const registros = await listarUsuarios(pool);
         registros ? (res.statusCode = 200) : (res.statusCode = 404);
         res.end(JSON.stringify(registros.rows));
       };
       listadoUsuarios(res, pool);
     }
     if (req.url.startsWith("/transferencia") && req.method == "POST") {
+      const nuevaTransferencia = (req, res, pool) => {
+        let body = "";
+        req.on("data", (t) => {
+          body += t;
+        });
+        req.on("end", async () => {
+          const transferencia = Object.values(JSON.parse(body));
+          const visual = await insertarTransferencia(transferencia, pool);
+          visual ? (res.statusCode = 201) : (res.statusCode = 500);
+          res.end();
+        });
+      };
       nuevaTransferencia(req, res, pool);
     }
     if (req.url.startsWith("/transferencias") && req.method == "GET") {
-      listaTransferencia(res, pool);
+      const listarTransferencia = async (res, pool) => {
+        const registros = await listarTransferencia(pool);
+        if (registros) {
+          res.statusCode = 200;
+          res.end(JSON.stringify(registros));
+        } else {
+          res.statusCode = 500;
+          res.end();
+        }
+      }
+      listarTransferencia(res, pool);
     }
   })
   .listen(3000, () => console.log("Servidor ON en puerto 3000"));
