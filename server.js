e28eb2c0fd128455aad154c95a11972c62720b2d
JuanQuezada <ju.quezada.c@gmail.com>
2022-01-23 01:59:44 -0300

    falta editar y transferencias

--- server.js
+++ server.js
@@ -30,21 +30,22 @@ async function insertarUsuario(datos, pool) {
     } catch (error_consulta) {
       console.log("! Error consulta !", error_consulta.code);
     }
-    release();
-    pool.end();
+    release()
   });
 }
 
 async function listarUsuarios() {
-    try {
-      const res = await pool.query("SELECT * FROM usuarios");
-      console.log("Registro actual:", res.rows);
-      return res
-    } catch (error_consulta) {
-      console.log("Error de conexion: ", error_consulta.code);
-    }    
+  try {
+    const res = await pool.query("SELECT * FROM usuarios");
+    console.log("Registro actual:", res.rows);
+    return res
+  } catch (error_consulta) {
+    console.log("Error de conexion: ", error_consulta.code);
+    return error_consulta;
+  }
 }
 async function editarUsuario(data) {
+  // pool.connect(async (error_conexion, client) => {
     const SQLQuery = {
       rowMode: "array",
       text: "UPDATE usuarios SET nombre = $2, balance = $3 WHERE id = $1 RETURNING *",
@@ -52,80 +53,70 @@ async function editarUsuario(data) {
     };
     try {
       const res = await pool.query(SQLQuery);
-      console.log(`Usuario ${res.rows[0].id} editado con éxito`);
-      return res
+      console.log("Registro Editado: ", res.rows);
+      return res.rows;
     } catch (error_consulta) {
       console.log("Error de conexion: ", error_consulta.code);
-      return error_consulta
     }
+  //   pool.end();
+  // });
 }
 
-// async function borrarUsuario(datos, pool) {
-//   pool.connect(async (error_conexion, client, release) => {
-//     if (error_conexion)
-//       return console.log("Error de conexion: ", error_conexion.code);
+async function borrarUsuario(id) {
+  //pool.connect(async (error_conexion, client) => {
+    try {
+      const res = await pool.query(`DELETE FROM usuarios WHERE id = ${id}`);
+      console.log("Usuario Eliminado: ", res.rows);
+      return res.rows;
+    } catch (error_consulta) {
+      console.log("Error de consulta: ", error_consulta.code);
+    }
+    //pool.end();
+  //});
+}
 
-//     const SQLQuery = {
-//       name: "eliminarUsuario",
-//       text: "DELETE FROM usuarios WHERE id = $1 RETURNING *",
-//       values: [datos],
-//     };
-//     try {
-//       const res = await client.query(SQLQuery);
-//       console.log(
-//         `Registro de usuario con id ${res.rows[0].id} eliminado con éxito`
-//       );
-//     } catch (error_consulta) {
-//       console.log("Error de consulta: ", error_consulta.code);
-//     }
-//     release();
+async function insertarTransferencia(datos, pool) {
+  pool.connect(async (error_conexion, client, release) => {
+    if (error_conexion)
+      return console.log("Error de conexion: ", error_conexion.code);
+    try {
+      await client.query("BEGIN");
+      const sumaSaldo = `UPDATE usuarios set balance = balance + ${datos[2]} where nombre = '${datos[1]}';`;
+      await client.query(sumaSaldo);
+      const restaSaldo = `UPDATE usuarios set balance = balance - ${datos[2]} where nombre = '${datos[0]}';`;
+      await client.query(restaSaldo);
+      const transfer = `insert into transferencias (emisor, receptor, monto, fecha) values ((select id from usuarios where nombre = '${nuevaTransferencia[0]}'), (select id from usuarios where nombre = '${nuevaTransferencia[1]}'), ${nuevaTransferencia[2]}, now());`;
+      await client.query(transfer);
+      await client.query("COMMIT");
+    } catch (error_consulta) {
+      console.log("Error de consulta: ", error_consulta.code);
+      await client.query("ROLLBACK");
+    }
+    release();
 
-//     pool.end();
-//   });
-// }
+    pool.end();
+  });
+}
 
-// async function insertarTransferencia(datos, pool) {
-//   pool.connect(async (error_conexion, client, release) => {
-//     if (error_conexion)
-//       return console.log("Error de conexion: ", error_conexion.code);
-//     try {
-//       await client.query("BEGIN");
-//       const sumaSaldo = `UPDATE usuarios set balance = balance + ${datos[2]} where nombre = '${datos[1]}';`;
-//       await client.query(sumaSaldo);
-//       const restaSaldo = `UPDATE usuarios set balance = balance - ${datos[2]} where nombre = '${datos[0]}';`;
-//       await client.query(restaSaldo);
-//       const transfer = `insert into transferencias (emisor, receptor, monto, fecha) values ((select id from usuarios where nombre = '${nuevaTransferencia[0]}'), (select id from usuarios where nombre = '${nuevaTransferencia[1]}'), ${nuevaTransferencia[2]}, now());`;
-//       await client.query(transfer);
-//       await client.query("COMMIT");
-//     } catch (error_consulta) {
-//       console.log("Error de consulta: ", error_consulta.code);
-//       await client.query("ROLLBACK");
-//     }
-//     release();
+async function consultaTransferencia() {
+  pool.connect(async (error_conexion, client, release) => {
+    if (error_conexion)
+      return console.log("Error de conexion: ", error_conexion.code);
+    try {
+      const consulta = {
+        rowMode: "array",
+        text: `SELECT (select nombre from usuarios where id = emisor) as emisor, (select nombre from usuarios where id = receptor) as receptor, monto, fecha FROM transferencias;`,
+      };
+      await client.query(consulta);
+    } catch (error_consulta) {
+      console.log("Error de consulta: ", error_consulta.code);
+      return error_consulta;
+    }
+    release();
 
-//     pool.end();
-//   });
-// }
-
-// async function consultaTransferencia() {
-//   pool.connect(async (error_conexion, client, release) => {
-//     if (error_conexion)
-//       return console.log("Error de conexion: ", error_conexion.code);
-//     try {
-//       const consulta = {
-//         rowMode: "array",
-//         text: `SELECT (select nombre from usuarios where id = emisor) as emisor, (select nombre from usuarios where id = receptor) as receptor, monto, fecha FROM transferencias;`,
-//       };
-//       await client.query(consulta);
-//     } catch (error_consulta) {
-//       console.log("Error de consulta: ", error_consulta.code);
-//       return error;
-//     }
-//     release();
-
-//     pool.end();
-//   });
-// }
+    pool.end();
+  });
+}
 
 http
   .createServer((req, res) => {
@@ -150,75 +141,75 @@ http
       };
       nuevoUsuario(req, res, pool);
     }
-    if (req.url.startsWith("/usuario?id=") && req.method === "PUT") {    
+    if (req.url.startsWith("/usuarios") && req.method === "GET") {
+      listarUsuarios()
+        .then((registros) => {
+          registros ? (res.statusCode = 200) : (res.statusCode = 404);
+          res.end(JSON.stringify(registros.rows));
+          console.log(registros);
+        })
+        .catch((err) => console.log(err));
+    }
+    if (req.url.startsWith("/usuario?id=") && req.method === "PUT") {
+      let body = "";
+      req.on("data", (payload) => {
+        body = JSON.parse(payload);
+      });
+      req.on("end", async () => {
+        data = [body.id, body.nombre, body.balance,];
+        const codigo = await editarUsuario(data);
+        if (codigo > 0) {
+          res.statusCode = 200;
+          texto = "Registro editado con éxito!";
+        } else {
+          res.statusCode = 400;
+          texto = "Error para editar el registro";
+        }
+        res.end(console.log(texto));
+      });
+    }
+    if (req.url.startsWith("/usuario?id=") && req.method === "DELETE") {
+      const { id } = url.parse(req.url, true).query;
+      const codigo = borrarUsuario(id, pool);
+      let texto = "";
+      if (codigo > 0) {
+        res.statusCode = 200;
+        texto = "Registro eliminado con éxito!";
+      } else {
+        res.statusCode = 400;
+        texto = "Error para eliminar el registro";
+      }
+      res.end(console.log(texto));
+    }
+
+    if (req.url.startsWith("/transferencia") && req.method == "POST") {
+      const nuevaTransferencia = (req, res, pool) => {
         let body = "";
         req.on("data", (payload) => {
           body = JSON.parse(payload);
         });
         req.on("end", async () => {
-          data = [body.id, body.nombre, body.balance,]
-          const codigo = await editarUsuario(data);
-          if (codigo > 0) {
-            res.statusCode = 200;
-            texto = "Registro editado con éxito!";
-          } else {
-            res.statusCode = 400;
-            texto = "Error para editar el registro";
-          }
-          res.end(console.log(texto));
+          transferencia = [body.emisor, body.receptor, body.monto];
+          const visual = await insertarTransferencia(transferencia, pool);
+          visual ? (res.statusCode = 201) : (res.statusCode = 500);
+          res.end();
         });
+      };
+      nuevaTransferencia(req, res, pool);
     }
-    // if (req.url.startsWith("/usuario") && req.method == "DELETE") {
-    //   const eliminarUsuario = async (req, res, pool) => {
-    //     const { id } = url.parse(req.url, true).query;
-    //     const codigo = await borrarUsuario(id, pool);
-    //     let texto = "";
-    //     if (codigo > 0) {
-    //       res.statusCode = 200;
-    //       texto = "Registro eliminado con éxito!";
-    //     } else {
-    //       res.statusCode = 400;
-    //       texto = "Error para eliminar el registro";
-    //     }
-    //     res.end(console.log(texto));
-    //   };
-    //   eliminarUsuario(req, res, pool);
-    // }
-    if (req.url.startsWith("/usuarios") && req.method === "GET") {     
-        listarUsuarios().then((registros) => {
-        registros ? (res.statusCode = 200) : (res.statusCode = 404); 
-        res.end(JSON.stringify(registros.rows)); 
-        console.log(registros);
-        }).catch(err => console.log(err))
+    if (req.url.startsWith("/transferencias") && req.method == "GET") {
+      const listarTransferencia = async (res, pool) => {
+        const registros = await consultaTransferencia(pool);
+        if (registros) {
+          res.statusCode = 200;
+          res.end(JSON.stringify(registros));
+        } else {
+          res.statusCode = 500;
+          res.end();
+        }
+      };
+      listarTransferencia(res, pool);
     }
-    // if (req.url.startsWith("/transferencia") && req.method == "POST") {
-    //   const nuevaTransferencia = (req, res, pool) => {
-    //     let body = "";
-    //     req.on("data", (payload) => {
-    //       body = JSON.parse(payload);
-    //     });
-    //     req.on("end", async () => {
-    //       transferencia = [body.emisor, body.receptor, body.monto];
-    //       const visual = await insertarTransferencia(transferencia, pool);
-    //       visual ? (res.statusCode = 201) : (res.statusCode = 500);
-    //       res.end();
-    //     });
-    //   };
-    //   nuevaTransferencia(req, res, pool);
-    // }
-    // if (req.url.startsWith("/transferencias") && req.method == "GET") {
-    //   const listarTransferencia = async (res, pool) => {
-    //     const registros = await consultaTransferencia(pool);
-    //     if (registros) {
-    //       res.statusCode = 200;
-    //       res.end(JSON.stringify(registros));
-    //     } else {
-    //       res.statusCode = 500;
-    //       res.end();
-    //     }
-    //   };
-    //   listarTransferencia(res, pool);
-    // }
   })
   .listen(3000, () => console.log("Servidor ON en puerto 3000"));
 
